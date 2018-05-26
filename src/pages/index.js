31f2026261993ec1ac623143ed235c0fc088d820
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-05-26 14:03:44 -0300

    List Drugs in IndexPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,52 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import FeaturedContent from '../components/FeaturedContent'
+import DrugCard from '../components/DrugCard'
 
-const IndexPage = () => (
-  <div>
-    <FeaturedContent tagline="Leia a bula." />
-    <section className="py-3 py-md-4">
-      <div className="container">
-        <h1>Psicoativos</h1>       
-      </div>
-    </section>
-  </div>
-)
+const IndexPage = ({ data }) => {
+  const drugs = data.drugs.edges
+  return (
+    <div>
+      <FeaturedContent tagline="Leia a bula." />
+      <section className="py-3 py-md-4">
+        <div className="container">
+          <h2>Psicoativos</h2>
+          <ul className="row list-unstyled mt-3">
+            {drugs.map((drug) =>
+              (
+                <li className="col-12 col-md-4">
+                  <DrugCard
+                    drug={drug.node.frontmatter}
+                  />
+                </li>
+              ))}
+          </ul>
+        </div>
+      </section>
+    </div>
+  )
+}
+
 
 export default IndexPage
+
+export const query = graphql`
+  query data {
+    drugs: allMarkdownRemark(
+      limit: 6
+      filter: {id: {regex: "/index/"} }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            name
+            class
+            path
+            molecules
+            aliases
+          }
+        }
+      }
+    }
+  }
+`
