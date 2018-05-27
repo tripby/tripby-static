55d5985b9764ceb90739d2bd1b9f6bdb66dbfece
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-05-27 14:50:16 -0300

    Add meta tags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,10 +8,18 @@ import '../assets/styles/main.scss'
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
-      meta={{
-        name: 'viewport', content: 'initial-scale=1.0, user-scalable=no',
-      }}
-    />
+      title="TRIPBY · Leia a bula"
+    >
+      <meta name="description" content="Entenda os efeitos, duração e dosagem das drogas psicoativas mais comuns. Use com consciência, pratique a redução de danos." />
+      <meta name="viewport" content="initial-scale=1.0, user-scalable=no, shrink-to-fit=no" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
+    </Helmet>
     <div className="row flex-column flex-md-row no-gutters justify-content-stretch" style={{ minHeight: '100vh' }}>
       <div className="col-12 col-md-3 col-lg-2">
         <Navigation />
