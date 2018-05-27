4b581e74fdffc27c960329a744511b5420b9c8dd
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-05-27 10:36:54 -0300

    Make AllArticles page

---
+++ src/pages/artigos/index.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import ArticleCard from '../../components/ArticleCard'
+
+const Articles = ({ data }) => {
+  const articles = data.allMarkdownRemark.edges
+  return (
+    <div>
+      <section className="py-3 py-md-4 bg-blueLighter">
+        <div className="container">
+          <h4 className="text-uppercase text-muted"><strong>Artigos</strong></h4>
+        </div>
+      </section>
+      <section className="py-3 py-md-4">
+        <div className="container">
+          <ul className="row list-unstyled mt-3">
+            {articles.map((article) =>
+              (
+                <li className="col-12 col-md-6">
+                  <ArticleCard
+                    article={article.node}
+                  />
+                </li>
+              ))}
+          </ul>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export const query = graphql`
+  query articles {
+    allMarkdownRemark(
+      limit: 1000
+      filter: {id: {regex: "/artigos/"} }  
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+export default Articles
