4cdb1f26acd9d366eeeb2a9426504be699081e85
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-05-27 15:53:24 -0300

    Unset molecule img height

--- src/components/DrugCard.js
+++ src/components/DrugCard.js
@@ -31,7 +31,6 @@ const DrugCard = ({ drug }) => (
                 top: 0,
                 left: 0,
                 width: '100%',
-                height: '100%',
               }}
             />
           </div>
