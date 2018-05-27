8cf6902ddd7110ff11185d18e502d56324758740
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-05-27 12:17:29 -0300

    Map overdose dosage

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,16 +7,21 @@ import '../assets/styles/main.scss'
 
 const Layout = ({ children, data }) => (
   <div>
+    <Helmet
+      meta={{
+        name: 'viewport', content: 'initial-scale=1.0, user-scalable=no',
+      }}
+    />
     <div className="row flex-column flex-md-row no-gutters justify-content-stretch" style={{ minHeight: '100vh' }}>
-        <div className="col-12 col-md-3 col-lg-2">
-          <Navigation />
-        </div>
-        <div className="col-12 col-md-9 col-lg-10 d-flex flex-column bg-greyLighter" style={{ flex: 1 }}>
-          <div style={{ flex: 1 }} className="d-flex flex-column">
-            {children()}
-          </div>
+      <div className="col-12 col-md-3 col-lg-2">
+        <Navigation />
+      </div>
+      <div className="col-12 col-md-9 col-lg-10 d-flex flex-column bg-greyLighter" style={{ flex: 1 }}>
+        <div style={{ flex: 1 }} className="d-flex flex-column">
+          {children()}
         </div>
       </div>
+    </div>
   </div>
 )
 
