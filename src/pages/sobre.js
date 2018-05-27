f63d965615a0afdeecd77dba3b935425048e144c
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-05-27 17:36:27 -0300

    Update about page

--- src/pages/sobre.js
+++ src/pages/sobre.js
@@ -4,12 +4,14 @@ const About = () => (
   <div>
     <section className="py-3 py-md-4 bg-blueLighter">
       <div className="container container--tight">
-        <h1>Sobre o Tripby</h1>
+        <h1 className="text-uppercase"><strong>Sobre o Tripby</strong></h1>
       </div>
     </section>
     <section className="py-3 py-md-4">
-      <div className="container">
-        <p>O Tripby é uma plataforma que reúne informações sobre substâncias psicoativas. Não condenamos nem promovemos o uso de drogas; nosso objetivo é difundir conhecimento pertinente ao uso dessas substâncias para fins de pesquisa e redução de danos.</p>
+      <div className="container container--tight">
+        <div className="blog-post">
+          <p>O Tripby é uma plataforma que reúne informações sobre substâncias psicoativas. Não condenamos nem promovemos o uso de drogas; nosso objetivo é difundir conhecimento pertinente ao uso dessas substâncias para fins de pesquisa e redução de danos.</p>
+        </div>
       </div>
     </section>
   </div>
