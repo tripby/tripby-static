7425aad02f02238335d176ee4d2bc7a1e7153ce6
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-08-11 20:42:48 -0400

    Install gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,8 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
