636d2eb06ef64e594991edc9b406df93aedd9a18
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-12-03 23:49:37 -0300

    Add undefined check for window in redux devtools setup

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -9,7 +9,9 @@ const createStore = () =>
   reduxCreateStore(
     rootReducer,
     /* eslint-disable no-underscore-dangle */
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    typeof window !== 'undefined' &&
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
     /* eslint-enable no-underscore-dangle */
   )
 
