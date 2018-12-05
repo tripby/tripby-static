1d8a2b4039611b035cb24503c8689d0ab47c9869
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-12-05 01:06:46 -0300

    Add colors into constants

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,17 @@
 export const apiUrl = 'https://api.graph.cool/simple/v1/tripby'
+
+export const colors = {
+  blue: '#00adee',
+  blueLight: '#4cf',
+  blueLighter: '#E0F7FA',
+  blueAccent: '#1DC0DC',
+  pink: '#ec297b',
+  pinkLight: '#f06292',
+  pinkLighter: '#F2ACC4',
+  green: '#4FFF88',
+  grey: ' #9E9E9E',
+  greyLight: '#eee',
+  greyLighter: '#fafafa',
+  greyDark: '#424242',
+  greyDarker: '#222',
+}
