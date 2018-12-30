337530562350e6b2f6da37020bbb6e062b305bbf
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-12-30 16:03:43 -0300

    Fix GATSBY_DOMAIN env

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const domain =
   process.env.NODE_ENV === 'production'
-    ? `https://${GATSBY_DOMAIN}`
+    ? `https://${process.env.GATSBY_DOMAIN}`
     : 'http://localhost:8000'
 
 export const apiUrl = 'https://api.graph.cool/simple/v1/tripby'
